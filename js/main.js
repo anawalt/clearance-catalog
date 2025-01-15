cbc432ee25901c69e6eccde3337ffd219a39c709
anawalt <DARBOLEAS@ANAWALTLUMBER.COM>
2025-01-15 06:41:50 -0800

    updated

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
-async function fetchCSV(url){
-    const res = await fetch(url);
+async function fetchCSV(url) {
+    let res;
+    try {
+        res = await fetch(url, {
+            cache: 'no-store'
+        });
+
+        if (!res.ok) {
+            throw new Error(`Response status: ${res.statusText}`);
+
+        }
+    } catch (error) {
+        console.error(error.message);
+        return false;
+    }
+
     const data = await res.text();
     const rows = data.split('\n');
-    
+
     const header = rows[0].trim().split(",");
 
     let dataArr = [];
-    
-    for(let i = 1; i < rows.length - 1; i++){
+
+    for (let i = 1; i < rows.length; i++) {
         let obj = {};
         const temp = rows[i].trim().split(",");
 
-        for(let y = 0; y < header.length; y++){
+        for (let y = 0; y < header.length; y++) {
             obj[header[y]] = temp[y];
         }
         dataArr.push(obj);
@@ -21,20 +35,71 @@ async function fetchCSV(url){
 }
 
 
-async function htmlGenerator(data){
-    let htmlString = `
-        <div class="card">
-            <img class="card-img-top" src="https://placehold.co/200x100" alt="Card image">
+function productCardGenerator(data) {
+    let finalImg = !data.img ? "system/placeholder.jpg" : data.img;
+
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('col', 'mb-5');
+    newDiv.innerHTML = `
+        <div class="card h-100 text-center">
+            <img loading="lazy" class="card-img-top" src="${window.location.origin}/anawalt-cc/img/${finalImg}" alt="Card image">
+	    <div class="card-header">
+	        <h4 class="card-title">${data.Description}</h4>
+		</div>
             <div class="card-body">
-                <h4 class="card-title">${data.Description}</h4>
-                <p class="card-text">${data.SKU}</p>
-                <p class="card-text">${data.QOH}</p>
-                <p class="card-text">${data.Retail}</p>
-                <a href="#" class="btn btn-primary">See Profile</a>
+                
+		<div class="row">
+		    <div class="col">
+			<p class="card-text fw-bold mb-1">SKU</p>
+ 		    	<p class="lead">${data.SKU}</p>
+		    </div>
+	            <div class="col">
+			<p class="card-text fw-bold mb-1">Quantity</p>
+ 		    	<p class="lead">${data.QOH}</p>
+		    </div>		
+		</div>
+                <h3>Price : $${data.Retail}</h3>
             </div>
         </div>
     `
-    const mainEl = document.querySelector("#main div");
-    mainEl.innerHTML = mainEl.innerHTML + htmlString;
+    return newDiv
+
 }
 
+function categoryCardGenerator(data) {
+    let finalImg = !data.img ? "system/placeholder.jpg" : data.img;
+
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('col', 'mb-5');
+    newDiv.innerHTML = `
+        <div class="card h-100">
+            <img loading="lazy" class="card-img-top" src="${window.location.origin}/anawalt-cc/img/category/${finalImg}" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">${data.name}</h4>
+                <a href="#" class="btn btn-success" data-src="${data.file}">View Products</a>
+            </div>
+        </div>
+    `
+    return newDiv
+}
+
+function menuGenerator(data){
+    const newLink = document.createElement('a');
+    newLink.href = "#";
+    newLink.classList.add('btn', 'btn-success', 'mx-2', 'my-2');
+    newLink.dataset.src = data.file;
+    newLink.innerText = data.name;
+    
+    return newLink;
+}
+
+function missingProducts(){
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('text-center');
+    newDiv.innerHTML = `
+        <img loading="lazy" width="125px" src="${window.location.origin}/anawalt-cc/img/system/warning.png" alt="warning icon">
+        <h2>No products available!</h2>
+    `
+
+    return newDiv;
+}
